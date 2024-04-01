2158d2e6995868f8ea255b2e169af5346d32ac9e
Muzaffar Rahimov <140341982+MuzaffarRakhimov@users.noreply.github.com>
2024-04-01 17:51:42 +0500

    new

--- src/frontend/nawbar/navbar.js
+++ src/frontend/nawbar/navbar.js
@@ -7,7 +7,7 @@ const NavbarApp = () => {
     
       <div className='navbor'>
       <Link to ={'/home'} className='link'> <h4>Assosiy </h4> </Link>
-      <Link to ={'/home2'} className='link'> <h4>Men hadqimda </h4> </Link>
+      <Link to ={'/home2'} className='link'> <h4>Men haqimda </h4> </Link>
       <Link to ={'/home3'} className='link'> <h4>O'qiganlarim</h4> </Link>
         
       </div>
